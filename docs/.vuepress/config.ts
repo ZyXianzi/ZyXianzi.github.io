2d96efdbb26ab77602c07aac7ed7347c26008d7b
Feifan Zheng <1450849885z@gmail.com>
2022-08-10 10:05:41 +0800

    update config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,18 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig } from 'vuepress';
+import { defaultTheme } from 'vuepress';
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '紫芋仙子的秘密基地',
   description: "ZyXianzi's blog",
-  base: '/blog/',
+  base: '/',
+  theme: defaultTheme({
+    home: '/',
+    repo: 'ZyXianzi/blog',
+    repoLabel: 'Github',
+    sidebar: [
+      '/README.md',
+      '/22-08-08.md',
+    ],
+  }),
 })
